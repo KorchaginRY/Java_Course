57b1bcce8281afae5d0d9a9886852c4fd03e9fde
Roman <rom25k@mail.ru>
2022-10-25 10:54:24 +1000

    HW_1

---
+++ program.java
@@ -0,0 +1,19 @@
+
+/**
+ * Написать программу вычисления n-ого треугольного числа
+ */
+import java.util.Scanner;
+
+public class program {
+
+    public static void main(String[] args) {
+        Scanner iScanner = new Scanner(System.in);
+        System.out.printf("Введите значение a = ");
+        int a = iScanner.nextInt();
+        double sum = (a * (a + 1)) / 2;
+        int n = (int) sum;
+        System.out.println(n);
+        iScanner.close();   
+    }
+}
+
